76fa803d9a1f161ebcc6312abc04b397c7edf920
jason4574 <janindumalshan259@gmail.com>
2025-09-25 21:05:25 +0530

    initial version

---
+++ src/matrix/Main.java
@@ -0,0 +1,118 @@
+package matrix;
+
+import java.util.Scanner;
+import matrix.Matrix;
+
+public class Main {
+    
+    static Scanner sc1 = new Scanner(System.in);
+    static Scanner sc2 = new Scanner(System.in);
+    static Matrix matrix_1;
+    static Matrix matrix_2;
+
+    public static void main(String[] args) {
+        while(true){
+            System.out.print("\n==== Matrix Cal [Any Dimention] ====\n\n1. Add\n2. Subtract\n3. Multiply\n4. Determinent\n5. Define matrix\n6. Exit\n\nChoice: ");
+            int choice = sc1.nextInt();
+            
+            if(choice==1){
+                if(checkMatrix()){
+                    Matrix result_matrix = matrix_1.add(matrix_2);
+                    if(result_matrix!=null){
+                        result_matrix.display();
+                    }
+                }
+            }else if(choice==2){
+                if(checkMatrix()){
+                    Matrix result_matrix = matrix_1.subtract(matrix_2);
+                    if(result_matrix!=null){
+                        result_matrix.display();
+                    }
+                }
+            }else if(choice==3){
+                if(checkMatrix()){
+                    Matrix result_matrix = matrix_1.multiplication(matrix_2);
+                    if(result_matrix!=null){
+                        result_matrix.display();
+                    }
+                }
+            }else if(choice==4){
+                if(checkMatrix()){
+                    if(isSquare()){
+                        System.out.print("\nEnter the matrix number (1 or 2): ");
+                        int num = sc1.nextInt();
+                        float det;
+                        if(num==1){
+                            det = matrix_1.calculateDet();
+                        }else{
+                            det = matrix_2.calculateDet();
+                        }
+                        
+                        System.out.printf("\nDeterminent: %.2f\n", det);
+                    }else{
+                        System.out.println("\nDeterminent can be calculated only for Square matrices");
+                    }
+                }
+            }else if(choice==5){
+                saveMatrix();
+            }else{
+                System.out.println("\nProgram End!!");
+                break;
+            }
+        }
+        
+    }
+
+    static void saveMatrix(){
+        System.out.print("\nEnter the dimention of matrix 1 (axb): ");
+        String inputVal[] = sc2.nextLine().split("x");
+        int R = Integer.parseInt(inputVal[0]);
+        int C = Integer.parseInt(inputVal[1]);
+
+        matrix_1 = new Matrix(R, C);
+
+        for(int i=0;i<R;i++){
+            System.out.printf("Enter row %d values: ", i+1);
+            String row[] = sc2.nextLine().split(" ");
+            for(int j=0; j<C; j++){
+                matrix_1.setValue(i, j, Integer.parseInt(row[j]));
+            }
+        }
+
+        System.out.print("\nEnter the dimention of matrix 2 (axb): ");
+        String inputVal2[] = sc2.nextLine().split("x");
+
+        R = Integer.parseInt(inputVal2[0]);
+        C = Integer.parseInt(inputVal2[1]);
+
+        matrix_2 = new Matrix(R, C);
+
+        for(int i=0;i<R;i++){
+            System.out.printf("Enter row %d values: ", i+1);
+            String row[] = sc2.nextLine().split(" ");
+            for(int j=0; j<C; j++){
+                matrix_2.setValue(i, j, Integer.parseInt(row[j]));
+            }
+        }
+
+    }
+
+    static boolean checkMatrix(){
+        if(matrix_1==null || matrix_2==null){
+            System.out.println("\nPlease define matrices first");
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    static boolean isSquare(){
+        if(matrix_1.rows == matrix_1.cols){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
+
---
+++ src/matrix/Matrix.java
@@ -0,0 +1,120 @@
+package matrix;
+
+public class Matrix {
+    int rows, cols;
+    float values[][];
+
+    Matrix(int r, int c) {
+        rows = r;
+        cols = c;
+        values = new float[r][c];
+    }
+
+    public void setValue(int r, int c, float value) {
+        values[r][c] = value;
+    }
+
+    public float getValue(int r, int c) {
+        return values[r][c];
+    }
+
+    public void display() {
+        System.out.println();
+        
+        for (int i = 0; i < rows; i++) {
+            System.out.print("|\t");
+            for (int j = 0; j < cols; j++) {
+                System.out.printf("%.2f", values[i][j] + "\t");
+            }
+            System.out.println("|");
+        }
+    }
+
+    public Matrix add(Matrix m) {
+        if(this.rows != m.rows || this.cols != m.cols) {
+            System.out.println("\nMatrices dimensions do not match for addition.");
+            return null;
+        }
+        Matrix result_matrix = new Matrix(this.rows, this.cols);
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<cols; j++) {
+                result_matrix.setValue(j, i, this.values[i][j] + m.values[i][j]);
+            }
+        }
+        return result_matrix;
+    }
+
+    public Matrix subtract(Matrix m) {
+        if(this.rows != m.rows || this.cols != m.cols) {
+            System.out.println("\nMatrices dimensions do not match for subtraction.");
+            return null;
+        }
+        Matrix result_matrix = new Matrix(this.rows, this.cols);
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<cols; j++) {
+                result_matrix.setValue(j, i, this.values[i][j] - m.values[i][j]);
+            }
+        }
+        return result_matrix;
+    }
+
+    public Matrix multiplication(Matrix m) {
+        if(this.cols != m.rows) {
+            System.out.println("\nMatrix dimensions do not match for multiplication");
+            return null;
+        }
+
+        Matrix result_matrix = new Matrix(this.rows, m.cols);
+
+        for (int i=0; i<this.rows; i++) {
+            for (int j=0; j<m.cols; j++) {
+                float sum=0, temp=0;
+                for(int z=0; z<this.cols;z++){
+                    temp = this.getValue(i, z) * m.getValue(z, j); 
+                    sum += temp;                   
+                }
+                
+                result_matrix.setValue(i, j, sum);
+            }
+        }
+        return result_matrix;
+
+    }
+
+    float calculateDet(){
+        // Used Gaussian elimination method
+
+        // Convert matrix to triangular matrix
+        for(int i=0;i<this.rows-1;i++){
+            float pivot = this.getValue(i, i);
+
+                
+            for(int j=i+1;j<this.rows;j++){
+                float[] currentRow = new float[this.cols];
+                System.arraycopy(values[i], 0, currentRow, 0, values[i].length);
+                
+                float[] nextRow = new float[this.cols];
+                System.arraycopy(values[j], 0, nextRow, 0, values[j].length);
+
+                float mul = values[j][i]/pivot;
+
+                for(int x=0;x<this.cols;x++){
+                    currentRow[x] = currentRow[x]* mul;
+                    nextRow[x] = nextRow[x] - currentRow[x];
+
+                    this.setValue(j, x, nextRow[x]);
+                }   
+
+            }
+        }
+
+        // Multiply diagonal values in the triangular matrix to calculate determinent
+        float det = 1;
+        for(int i=0;i<this.rows;i++){
+            det *= values[i][i];
+        }
+
+        return det;
+    }
+
+}
